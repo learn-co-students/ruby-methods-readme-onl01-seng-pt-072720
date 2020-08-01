a2597f504a8d6c7410afd7f00f073786d0cd998a
C. Luke Murphree <clukemurphree@icloud.com>
2020-08-01 03:25:30 +0000

    Done.

--- lib/example.rb
+++ lib/example.rb
@@ -11,3 +11,123 @@ puts phrase
 puts phrase
 puts phrase
 puts phrase
+[00:50:58] ~
+// ♥ learn open ruby-methods-readme-onl01-seng-pt-072720
+Looking for lesson...
+Forking lesson...
+Cloning lesson...
+Opening lesson...
+Done.
+[00:51:03] (master) ruby-methods-readme-onl01-seng-pt-072720
+// ♥ irb
+2.6.1 :001 > ruby lib/example.rb to see what it produces.
+2.6.1 :002 >
+2.6.1 :003 >   ruby lib/example.rb
+Traceback (most recent call last):
+        3: from /usr/local/rvm/rubies/ruby-2.6.1/bin/irb:23:in `<main>'
+        2: from /usr/local/rvm/rubies/ruby-2.6.1/bin/irb:23:in `load'
+        1: from /usr/local/rvm/rubies/ruby-2.6.1/lib/ruby/gems/2.6.0/gems/irb-1.0.0/exe/irb:11:in `<top (required)>'
+SyntaxError ((irb):1: syntax error, unexpected tIDENTIFIER, expecting end-of-input)
+ruby lib/example.rb to see what it produces.
+                    ^~
+2.6.1 :004 > ruby lib/example.rb
+Traceback (most recent call last):
+        5: from /usr/local/rvm/rubies/ruby-2.6.1/bin/irb:23:in `<main>'
+        4: from /usr/local/rvm/rubies/ruby-2.6.1/bin/irb:23:in `load'
+        3: from /usr/local/rvm/rubies/ruby-2.6.1/lib/ruby/gems/2.6.0/gems/irb-1.0.0/exe/irb:11:in `<top (required)>'
+        2: from (irb):4
+        1: from (irb):4:in `rescue in irb_binding'
+NameError (undefined local variable or method `lib' for main:Object)
+2.6.1 :005 > def greeting
+2.6.1 :006?>    puts "Hello World"
+2.6.1 :007?>   end
+ => :greeting
+2.6.1 :008 > greeting
+Hello World
+ => nil
+2.6.1 :009 > exit
+[02:37:27] (master) ruby-methods-readme-onl01-seng-pt-072720
+// ♥ exit irb
+logout
+bash: exit: irb: numeric argument required
+[02:39:33] ~
+// ♥ touch greeting.rb
+[02:40:16] ~
+// ♥ greeting.rb
+bash: greeting.rb: command not found
+[02:40:28] ~
+// ♥ def greeting
+bash: def: command not found
+[02:41:24] ~
+// ♥  touch greeting.rb
+[02:41:40] ~
+// ♥ def "greeting"
+bash: def: command not found
+[02:41:50] ~
+// ♥ def greeting
+bash: def: command not found
+[02:42:08] ~
+// ♥ ruby greeting.rb
+[02:44:32] ~
+// ♥ ruby greeting.rb
+[02:45:34] ~
+// ♥ def greeting.rb
+bash: def: command not found
+[02:48:33] ~
+// ♥ ruby greeting.rb
+[02:50:46] ~
+// ♥ ruby greeting.rb
+[02:51:21] ~
+// ♥ ruby lib/example.rb
+Traceback (most recent call last):
+ruby: No such file or directory -- lib/example.rb (LoadError)
+[02:53:34] ~
+// ♥ exit irb
+logout
+bash: exit: irb: numeric argument required
+Opening the lesson…please do not refresh the page[02:55:06] ~
+// ♥ learn open ruby-methods-readme-onl01-seng-pt-072720
+Looking for lesson...
+Forking lesson...
+Cloning lesson...
+Opening lesson...
+Done.
+[02:55:10] (master) ruby-methods-readme-onl01-seng-pt-072720
+// ♥ ruby greeting.rb
+Hello World
+[02:56:40] (master) ruby-methods-readme-onl01-seng-pt-072720
+// ♥ $ ruby greeting.rb
+bash: $: command not found
+[02:57:16] (master) ruby-methods-readme-onl01-seng-pt-072720
+// ♥ ruby greeting.rb
+Hello World
+[02:57:36] (master) ruby-methods-readme-onl01-seng-pt-072720
+// ♥ ruby greeting.rb
+Hello World
+Hello World
+Hello World
+Hello World
+Hello World
+[02:58:16] (master) ruby-methods-readme-onl01-seng-pt-072720
+// ♥ greeting
+bash: greeting: command not found
+[02:58:22] (master) ruby-methods-readme-onl01-seng-pt-072720
+// ♥ ruby.greeting.rb
+bash: ruby.greeting.rb: command not found
+[02:58:56] (master) ruby-methods-readme-onl01-seng-pt-072720
+// ♥ ruby greeting.rb
+[03:02:52] (master) ruby-methods-readme-onl01-seng-pt-072720
+// ♥ ruby greeting.rb
+Hello World
+[03:03:20] (master) ruby-methods-readme-onl01-seng-pt-072720
+// ♥ ruby greeting.rb
+Hello World
+Hello World
+Hello World
+Hello World
+Hello World
+[03:03:51] (master) ruby-methods-readme-onl01-seng-pt-072720
+// ♥ greeting
+bash: greeting: command not found
+[03:03:58] (master) ruby-methods-readme-onl01-seng-pt-072720
+// ♥submit lesson
